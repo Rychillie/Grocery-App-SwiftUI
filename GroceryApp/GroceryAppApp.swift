f13070d246bba16ddd3708f35f3b7676ad98be1a
Rychillie <rychillie@rychillie.net>
2024-11-23 13:28:50 -0300

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Rychillie Umpierre de Oliveira on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
